4328617f63f4646c3ad2aa017b28b6feb131958d
Ralph <rama.o.ralph@gmail.com>
2016-01-08 21:17:35 -0500

    Began work on a new game!

---
+++ beat-them-up/js/boot.js
@@ -0,0 +1,33 @@
+var BeatThemUp = {}
+
+BeatThemUp.boot = function( game )
+{
+
+}
+
+BasicGame.Boot.prototype =
+{
+  init: function()
+  {
+    // disallow multi-touch
+    this.input.maxPointers = 1;
+
+    // pause the game if our browser tab loses focus
+    this.stage.disableVisibilityChange = true;
+  },
+
+  preload: function()
+  {
+    // load assets required for the preloader
+    this.load.image( 'preloadBG',  'img/preload/preloadBG.png' )
+    this.load.image( 'preloadBar', 'img/preload/preloadBar.png' )
+  },
+
+  create: function()
+  {
+    // change the background color to match the page color
+    this.stage.backgroundColor = "#CCCCCC"
+    // Start the preloader since we've already got everything we need set up
+    this.state.start('preloader')
+  }
+}
